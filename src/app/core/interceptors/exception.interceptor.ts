b249da489a74453c6225df80aa7656604bd43f8f
Pedro Lacombe <pedrocarlantonio@gmail.com>
2022-01-15 05:04:47 -0300

    Core module, to be loaded whe user access the first page

---
+++ src/app/core/interceptors/exception.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ExceptionInterceptor implements HttpInterceptor {
+
+  constructor(private toastr: ToastrService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.toastr.error(error.message, `${error.status} - ${error.statusText}`);
+        return throwError(() => error);
+      })
+    );
+  }
+
+}
