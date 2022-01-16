8fcc457d14b07a588e20d5704d9ce1b369b9976a
Pedro Lacombe <pedrocarlantonio@gmail.com>
2022-01-16 01:33:45 -0300

    Added some login if path variable is not a number

--- src/app/features/courses/course.resolver.ts
+++ src/app/features/courses/course.resolver.ts
@@ -2,21 +2,41 @@ import { Injectable } from '@angular/core';
 import {
   Resolve,
   RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot,
+  Router
 } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Course } from './course';
 import { CoursesService } from './courses.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CourseResolver implements Resolve<Course> {
+export class CourseResolver implements Resolve<Course | null> {
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(
+    private router: Router,
+    private coursesService: CoursesService
+  ) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-    const id = route.paramMap.get('id');
-    return this.coursesService.findById(id!);
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course | null> {
+    const id = route.paramMap.get('id')!;
+
+    if (isNaN(+id)) {
+      this.navigateToCoursesUrl();
+      return of(null);
+    }
+
+    return this.coursesService.findById(+id!).pipe(
+      catchError(() => {
+        this.navigateToCoursesUrl();
+        return of(null);
+      })
+    );
   }
+
+  private navigateToCoursesUrl() {
+    this.router.navigateByUrl("/courses");
+  }
+
 }
